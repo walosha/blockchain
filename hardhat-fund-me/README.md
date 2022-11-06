06c5d15ddcdfeafdcd525ecd465a9e4e443440e7
olawale afuye <walosha@yahoo.com>
2022-11-07 00:13:06 +0100

    INITIALIZED HARDHAT-FUND-ME

---
+++ hardhat-fund-me/README.md
@@ -0,0 +1,16 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+### packages 
+@chainlink/contracts @nomicfoundation/hardhat-chai-matchers @nomiclabs/hardhat-ethers @nomiclabs/hardhat-etherscan chai dotenv ethers hardhat-deploy hardhat-gas-reporter prettier-plugin-solidity solidity-coverage
--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -2,12 +2,12 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("hardhat-gas-reporter")
-
+require("solidity-coverage")
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 module.exports = {
