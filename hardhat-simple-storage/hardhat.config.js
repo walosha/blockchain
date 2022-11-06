6b3ca24abbb0ddb24aeb46068a359549c4f90e5f
olawale afuye <walosha@yahoo.com>
2022-11-06 23:10:56 +0100

    ADDED TEST TO SIMPLE STORAGE

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,10 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
     solidity: "0.8.17",
     networks: {
@@ -13,5 +16,14 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 5,
         },
+
+        localhost: {
+            //accounts will be provide by hardhat
+            url: "http://127.0.0.1:8545",
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
 }
--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,13 +1,46 @@
-const { ethers } = require("hardhat")
+// imports
+const { ethers, run, network } = require("hardhat")
 
+// async main
 async function main() {
-    const SimpleStorage = await ethers.getContractFactory("SimpleStorage")
-    const [deployer] = await ethers.getSigners()
-
-    const simpleStorage = await SimpleStorage.deploy()
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    // what happens when we deploy to our hardhat network?
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for 6 block confirmations...")
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
 
-    console.log(`SimpleStorage is being deployed to ${simpleStorage.address}`)
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
+}
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
